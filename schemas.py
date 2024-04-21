2eb24577574f6f3cc1e33b27918049cdb38b9abc
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2024-04-21 16:23:08 -0300

    Adiciona schemas

--- schemas.py
+++ schemas.py
@@ -15,3 +15,40 @@ class ItemResponse(BaseModel):
 	id: int
 	name: str
 	description: str
+
+
+# Pydantic model for request data
+class InstitutionCreate(BaseModel):
+	name: str
+	address: str
+	latitude: str
+	longitude: str
+	responsibleUserId: int
+	created: str
+	visitTime: str
+	visitDates: str
+	
+# Pydantic model for request data
+class InstitutionUpdate(BaseModel):
+	name: str
+	address: str
+	latitude: str
+	longitude: str
+	responsibleUserId: int
+	created: str
+	visitTime: str
+	visitDates: str
+
+# Pydantic model for response data
+class InstitutionResponse(BaseModel):
+	id: int
+	name: str
+	address: str
+	latitude: str
+	longitude: str
+	responsibleUserId: int
+	created: str
+	visitTime: str
+	visitDates: str
+	
+
